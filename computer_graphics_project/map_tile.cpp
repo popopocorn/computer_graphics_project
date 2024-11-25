70af046904ca5dbbc19b33a0e355ef4f98cf7e17
popopocorn <popcorn375@naver.com>
2024-11-25 23:27:45 +0900

    aabb 제작 함수

--- computer_graphics_project/map_tile.cpp
+++ computer_graphics_project/map_tile.cpp
@@ -10,6 +10,7 @@
 #include<string>
 
 
+
 /*
 	float x;
 	float y;
@@ -35,6 +36,7 @@ MapTile::MapTile(float in_x, float in_y, float in_z, char* m, char* t) {
 	z = in_z;
 	read_obj_file(m, &model);
 	type = t;
+	box = make_aabb(model);
 
 
 }
@@ -47,4 +49,35 @@ aabb MapTile::get_aabb() {
 void MapTile::update_position() {
 	trans = glm::mat4(1.0f);
 	trans = glm::translate(trans, glm::vec3(x, y, z));
+}
+
+aabb make_aabb(const Model m) { // aabb를 만들어 주는 함수, 모델에서 x, y, z각각의 최대, 최소값을 찾아 리턴
+	float min_x = std::numeric_limits<float>::max();
+	float max_x = std::numeric_limits<float>::min();
+	float min_y = std::numeric_limits<float>::max();
+	float max_y = std::numeric_limits<float>::min();
+	float min_z = std::numeric_limits<float>::max();
+	float max_z = std::numeric_limits<float>::min();
+
+	for (int i = 0; i < m.vertex_count; ++i) {
+		min_x = std::min(min_x, m.vertices[i].x);
+		max_x = std::max(min_x, m.vertices[i].x);
+
+		min_y = std::min(min_y, m.vertices[i].y);
+		max_y = std::max(min_y, m.vertices[i].y);
+
+		min_z = std::min(min_z, m.vertices[i].z);
+		max_z = std::max(min_z, m.vertices[i].z);
+	}
+	aabb temp = {
+		min_x,
+		max_x,
+
+		min_y,
+		max_y,
+
+		min_z,
+		max_z,
+	};
+	return temp;
 }
